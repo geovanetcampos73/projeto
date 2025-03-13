10e5614277831e12d0e96dadb7936fc7db537f13
Geovane Campos <geovanetcampos73@gmail.com>
2025-03-13 15:43:55 -0300

    Alteracao de alert para console.log

--- ola.js
+++ ola.js
@@ -1,7 +1,7 @@
-alert('Olá mundo');
-alert('teste de git'); 
+console.log('Olá mundo');
+console.log('teste de git'); 
 let a="100";
 let b="20";
-alert('Mensagem de teste:',a);
-alert('mensagem de teste:',b);
-alert('Teste de alteração');
+console.log('Mensagem de teste:',a);
+console.log('mensagem de teste:',b);
+console.log('Teste de alteração');
